669d650dfb58eb76e2486d44c60807e1503685ff
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2024-11-20 21:07:18 -0300

    cambios responsive

--- src/pages/Home/components/ProductsSection/ProductsSection.jsx
+++ src/pages/Home/components/ProductsSection/ProductsSection.jsx
@@ -6,6 +6,7 @@ import gsap from 'gsap';
 import ScrollTrigger from 'gsap/ScrollTrigger';
 import { PreloaderContext } from "../../../../context/PreloaderContext";
 
+
 gsap.registerPlugin(ScrollTrigger);
 
 
