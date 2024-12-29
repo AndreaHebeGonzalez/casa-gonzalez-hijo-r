bb3a7870c72a5ca555825cdeded6e460b69ead86
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2024-12-28 22:05:06 -0300

    error deploy

--- vite.config.js
+++ vite.config.js
@@ -16,9 +16,7 @@ export default defineConfig({
   },
   build: {
     outDir: 'dist', // Asegura que la salida de la build esté en la carpeta 'dist'
-    assetsDir: 'assets', // Si quieres que los archivos estáticos vayan a una carpeta 'assets'
     sourcemap: true, // Habilita los sourcemaps en producción para depuración
-    cssCodeSplit: false, // No dividir el CSS en archivos más pequeños
   },
   plugins: [react()],
 })
