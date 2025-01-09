fc52600a46ca0f5f8bcab9f836fd62d732d0a9f0
AndreaHebeGonzalez <andreagzlzhebe2907@gmail.com>
2025-01-09 01:27:01 -0300

    ajusto data-scroll-speed en AboutInformation.jsx

--- src/pages/Home/components/AboutSection/AboutInformation.jsx
+++ src/pages/Home/components/AboutSection/AboutInformation.jsx
@@ -18,7 +18,7 @@ export const AboutInformation = () => {
   return (
     <div className="about-s__information">
       <div className="about-s__content">
-        <div className="about-s__text" data-scroll data-scroll-speed="1">
+        <div className="about-s__text" data-scroll data-scroll-speed="0.5">
           <p>En Casa González e Hijo, nos especializamos en el desarrollo, diseño, fabricación y comercialización de fusiles de precisión de la más alta calidad. Cada componente de nuestros fusiles es fabricado en nuestras instalaciones, utilizando procesos de mecanizado CNC de última generación y software de diseño por computadora. Esta combinación de tecnología avanzada e ingeniería nos permite ofrecer productos que cumplen con los estándares más exigentes de precisión y fiabilidad.</p>
         </div>
         
